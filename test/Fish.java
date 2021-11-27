a7224245561dc7ce22a02fd7e149f4a6e407a64e
HassenSamir <shassen092@gmail.com>
2021-11-27 17:56:21 +0100

    last friday version

---
+++ test/Fish.java
@@ -0,0 +1,90 @@
+package test;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class Fish {
+    float x;
+    float y;
+    Color r;
+    int dia=10;
+    float speed = 4.0f;
+    Main m;
+    File file;
+    String path;
+    BufferedImage img;
+
+
+    public Fish(Main m, int i, int length) throws IOException {
+        this.m = m;
+        Random r = new Random();
+        x = r.nextInt(500-dia);
+         y = r.nextInt(500-dia);
+        // x = i + dia * 2;
+        //y = i + dia * 2;
+
+        this.r = Color.RED;
+         path = "C:\\Users\\shass\\Downloads\\fish-removebg-preview.png";
+        file = new File(path);
+        img = ImageIO.read(file);
+        img = scale(img, 20, 20);
+
+
+    }
+
+    public void move(int desX, int desY){
+        float dx = desX - x;
+        float dy = desY - y;
+        if(dx ==0 && dy == 0) return;
+        float dir = (float) Math.atan2(dx, dy) ;
+
+
+        if(m.getInverse()){
+            x = x - (float) (speed*Math.cos(dir) - 1);
+            y = y + (float) (speed*Math.sin(dir) - 1);
+        }else{
+            x = x + (float) (speed*Math.cos(dir) - 1);
+            y = y - (float) (speed*Math.sin(dir) - 1);
+        }
+
+
+    }
+
+    public void paint(Graphics g) throws IOException {
+
+        //BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        g.drawImage(img,(int)x , (int)y, null);
+        //g.fillOval((int)x , (int)y , dia, dia);
+
+    }
+
+    public static BufferedImage scale(BufferedImage src, int w, int h)
+    {
+        BufferedImage img =
+                new BufferedImage(w, h, BufferedImage.TRANSLUCENT);
+        int x, y;
+        int ww = src.getWidth();
+        int hh = src.getHeight();
+        int[] ys = new int[h];
+        for (y = 0; y < h; y++)
+            ys[y] = y * hh / h;
+        for (x = 0; x < w; x++) {
+            int newX = x * ww / w;
+            for (y = 0; y < h; y++) {
+                int col = src.getRGB(newX, ys[y]);
+                img.setRGB(x, y, col);
+            }
+        }
+        return img;
+    }
+
+
+
+}
--- test/Main.java
+++ test/Main.java
@@ -2,55 +2,72 @@ package test;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main extends JPanel {
 
-    static int width = 800;
-    static int height = 800;
+    static int width = 1200;
+    static int height = 1200;
 
-    private static int numberOfBalls = 500;
+    private static int numberOfFish = 1000;
+    public boolean inverse = false;
 
-    static ArrayList<Ball> balls = new ArrayList<Ball>(numberOfBalls);
+    static ArrayList<Fish> fish = new ArrayList<Fish>(numberOfFish);
     public Main() {
         setVisible(true);
     }
 
-    public static void main(String[] args) throws InterruptedException{
-        JFrame f = new JFrame("Hello");
+    public static void main(String[] args) throws InterruptedException, IOException {
+        JFrame f = new JFrame("Fish Bank");
         f.setSize(width, height);
         Main obj = new Main();
         obj.addMouseListener(new MyListener(obj));
         f.add(obj);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        for(int i=0; i < numberOfBalls; i++){
-            Ball b = new Ball(obj);
-            balls.add(b);
+        f.getContentPane().setBackground(new Color(5, 65, 90));
+
+        for(int i=0; i < numberOfFish; i++){
+            Fish b = new Fish(obj, i, numberOfFish);
+            fish.add(b);
         }
         while (true){
             obj.repaint();
             Thread.sleep(10);
         }
+        
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        for(Ball b:balls)
-            b.paint(g);
+        for(Fish b:fish) {
+            try {
+                b.paint(g);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         g.setColor(Color.GREEN);
-        g.fillOval(MyListener.x, MyListener.y, 10, 10);
+        g.fillOval(400, 400, 10, 10);
         update();
     }
 
     public void update(){
-        for(Ball b:balls)
-            b.move(400, 400);
+        for(Fish b:fish)
+            b.move(600, 600);
     }
 
+    public boolean getInverse(){
+        return this.inverse;
+    }
+
+    public void setInverse(){
+        this.inverse = !inverse;
+    }
 
 
 }
