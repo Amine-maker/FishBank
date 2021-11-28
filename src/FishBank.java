7d0ea6b81846f8e77d9f1d9936f7f4aa796544cc
amine djennadi <amine.djennadi@hotmail.fr>
2021-11-28 14:03:23 +0100

    amine

---
+++ src/FishBank.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class FishBank {
+
+    private ArrayList<Fish> fish = new ArrayList<Fish>();
+    private int height;
+    private int width;
+    private int angle;
+
+    public FishBank(int number) {
+    }
+
+    public ArrayList<Fish> getFish() {
+        return fish;
+    }
+
+    public void setFish(ArrayList<Fish> fish) {
+        this.fish = fish;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
+    }
+
+}
--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-  int test = 5;
-
-  public int getTest() {
-    return this.test;
-  }
-}
